7aa91e2657892a3f60b279ea748e37866688f10f
Yvan Brunel <8brunel@gmail.com>
2024-03-28 13:45:29 -0400

    Updated again

--- script.js
+++ script.js
@@ -2,82 +2,90 @@
 let heartRateData;
 let dataArray;
 let particles = [];
-let currentColor;
-let dateTime;
 
-let colors;
 
-let allParticlesOffScreen = false; // Flag to track if all particles have moved off the canvas
-
+let currentFileIndex = 0; // Start with the first file
+let fileNames = [
+  'AllHearRates/heart_rate-2023-03-11.json',
+  'AllHearRates/heart_rate-2024-01-04.json',
+  // Add more file names as needed
+];
 
 function preload() {
     //dataArray = loadJSON('AllHearRates/heart_rate-2023-03-11.json'); //confidence is mainly 2
-    dataArray = loadJSON('AllHearRates/heart_rate-2024-01-04.json');
+    //dataArray = loadJSON('AllHearRates/heart_rate-2024-01-24.json');
+    //dataArray = loadJSON('AllHearRates/heart_rate-2024-01-24.json');
     //dataArray = loadJSON('AllHearRates/heart_rate-2024-02-04.json');
-    //dataArray = loadJSON('sampleJSON.json');
+    
+    dataArray = loadJSON(fileNames[currentFileIndex]); // Load the current file
+
 }
 function setup() {
-  //createCanvas(800, 800);
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1800, 1600);
+  //createCanvas(windowWidth, windowHeight);
   if (dataArray !== null && typeof dataArray === 'object') {
-    heartRateData = Object.values(dataArray);
+  heartRateData = Object.values(dataArray);
+  // Call the createParticles function to create particles for the current file
+  createParticles();
+  } else {
+  console.error('heartRateData is undefined or null');
+  }
+  frameRate(60);
+  }
 
-    console.log(heartRateData[0].dateTime);
-
-    dateTime = new Date(heartRateData[0].dateTime);
-
-    
-    // create particles for each data point
-    for (let i = 0; i < heartRateData.length; i++) {
+function createParticles() {
+  // create particles for each data point
+    for (let i = 0; i < heartRateData.length; i++) 
+    {
       let bpm = heartRateData[i].value.bpm;
       let confidence = heartRateData[i].value.confidence;
       let p = new Particle(bpm, confidence);
       particles.push(p);
     }
-    // Call draw() at least once to display the particles
-    draw();
-
-    // Stop the loop if all particles have moved off the canvas
-    if (allParticlesOffScreen) {
-      console.log("Number of red: ",nbRed, " Orange:", nbOrange ," blue: ", nbLB, " back: ", nbBlack);
-      noLoop();
-    }
-    } else {
-    console.error('heartRateData is undefined or null');
-    }
-}
-
-let frameCounter = 0; // Frame counter
+  }
+ 
+let frameCounter = 0;
 function draw() {
-  if (!allParticlesOffScreen) {
-    
-    //console.log(frameCounter);
-    background(105);// to remove trail
+background(105); // to remove trail
 
-    // Update and display each particle
-    for (let j = particles.length - 1; j >= 0; j--) {
-      particles[j].update();
-      particles[j].display();
+// Update and display each particle
+for (let j = particles.length - 1; j >= 0; j--) {
+particles[j].update();
 
-      // Remove particles that have gone off-screen
-      if (particles[j].isOffScreen()) {
-        particles.splice(j, 1);
-      }
-    }
- // Increment frame counter
- frameCounter++;
 
- // Check if frame counter has reached 200
- if (frameCounter >= 130) {  
+particles[j].display();
+
+// Remove particles that have gone off-screen
+if (particles[j].isOffScreen()) {
+particles.splice(j, 1);
+}
+}
+
+// Increment frame counter
+frameCounter++;
+
+// Check if frame counter has reached 120
+if (frameCounter >= 120) {
+ 
+  // Determine the maximum value
+let maxValue = Math.max(nbRed, nbBlack, nbLB, nbOrange);
+
+// Find out which variable has the maximum value
+let maxName;
+switch (maxValue) {
+    case nbRed: maxName = 'nbRed'; break;
+    case nbBlack: maxName = 'nbBlack'; break;
+    case nbLB: maxName = 'nbLB'; break;
+    case nbOrange: maxName = 'nbOrange'; break;
+}
+
+  noLoop();
   console.log("Number of red: ",nbRed, " Orange:", nbOrange ," blue: ", nbLB, " back: ", nbBlack);
-   allParticlesOffScreen = true;
- }
 }
 }
-
 
 class Particle {
-  constructor(bpm, confidence) {
+  constructor(bpm) {
     // this.position = createVector(width / 2, height / 2); // Start at the center of the canvas
     // this.velocity = p5.Vector.fromAngle(random(TWO_PI), map(bpm, 0, 200, 1, 5)); // Calculate a random velocity based on the bpm and a random angle
     
@@ -86,7 +94,7 @@ class Particle {
 
     this.position = createVector(width / 2, height / 2);
     this.velocity = p5.Vector.random2D();
-    this.velocity.mult(map(bpm, 40, 170, 1, 5));
+    this.velocity.mult(map(bpm, 40, 190, 1, 5));
     this.acceleration = createVector(0, 0);
     this.bpm = bpm;
     this.color = getColor(bpm);
@@ -121,19 +129,19 @@ function getColor(bpm) {
     nbRed++;
     return color(255, 0, 0); // red for very fast heart rate
   } 
-  else if (bpm >= 100) 
+  else if (bpm >= 85) 
   {
     nbOrange++;
     return color(255, 140, 0); // orange for medium heart rate
   } 
-  else if (bpm >= 60) 
+  else if (bpm >= 55) 
   {
     nbLB++;
     return color(0, 191, 255); // light blue for slow heart rate
   } else 
   {
     nbBlack++;
-    return color(0); // black as default color
+    return color(0, 0, 128); // black as default color
   }
 }
 
